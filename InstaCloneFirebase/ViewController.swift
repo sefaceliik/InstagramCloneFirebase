2a8484bd8e1669e05919f5dd26acaf5309b253ba
sefacelik <sefacelik1800@gmail.com>
2020-05-08 23:08:17 +0300

    Initial Commit

---
+++ InstaCloneFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Sefa Çelik on 8.05.2020.
+//  Copyright © 2020 Sefa Celik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
